91d53e164534e226d31cf8e890d7925c7727101c
sityoy12 <putraanimas@gmail.com>
2022-02-22 20:47:24 +0700

    oke

--- TioIrfanAntoni_freeclass_eduwork/tugas_php/array.php
+++ TioIrfanAntoni_freeclass_eduwork/tugas_php/array.php
@@ -1,26 +1,4 @@
-<?php
-    $array = array(
-        array(
-            'nama' => 'Tio',
-            'umur' => 21,
-            'kelas' => 'Laravel'
-        ),
-        array(
-            'nama' => 'Irfan',
-            'umur' => 25,
-            'kelas' => 'React.js'
-        ),
-        array(
-            'nama' => 'Antoni',
-            'umur' => 28,
-            'kelas' => 'Python'
-        ),
-    );
-    foreach ($array as $key => $value){
-        echo  "Nama : ".$value['nama']. ", umur :".$value['umur']. ', kelas :'.$value['kelas'];
-        echo "<br>";
-    }
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -35,15 +13,15 @@
 /* Created by Daniel Ok */
 
 function grade($nilai)
-{
- if($nilai <= 100 ) { $grade = "A"; }
- if($nilai <  80 )  { $grade = "B"; }
- if($nilai <  70 )  { $grade = "C"; }
- if($nilai <  60 )  { $grade = "D"; }
- if($nilai <  50 )  { $grade = "E"; }
+(
+ if($nilai <= 100 ) ( $grade = "A"; )
+ if($nilai <  80 )  ( $grade = "B"; )
+ if($nilai <  70 )  ( $grade = "C"; )
+ if($nilai <  60 )  ( $grade = "D"; )
+ if($nilai <  50 )  ( $grade = "E"; )
 
  return $grade;
-}
+);
 ?>
 
 <h2>Data Nilai </h2>
@@ -58,6 +36,54 @@ function grade($nilai)
  <th width="100">Nilai</th>
  <th width="100">Grade</th>
  </tr>
- 
+ <?php
+ $data = array (
+     "0" => array (
+		"nama" : "Pelita",
+		"kelas" : "Laravel",
+		"alamat" : "Bandung",
+		"tanggal_lahir" : "1997-12-27",
+		"nilai" : 90
+     ),
+	"1" => array (
+		"nama" : "Najmina",
+		"kelas" : "Vue JS",
+		"alamat" : "Jakarta",
+		"tanggal_lahir" : "1998-10-07",
+		"nilai" : 55
+    ),
+    "2" => array (
+		"nama" : "Anita",
+		"kelas" : "Design",
+		"alamat" : "Semarang",
+		"tanggal_lahir" : "1999-08-20",
+		"nilai" : 80
+    ),
+	"3" => array (
+		"nama" : "Bayu",
+		"kelas" : "Digital Marketing",
+		"alamat" : "Bandung",
+		"tanggal_lahir" : "1990-01-01",
+		"nilai" : 65
+	),
+	"4" => array (
+		"nama" : "Nasa",
+		"kelas" : "UI/UX Designer",
+		"alamat" : "Bandung",
+		"tanggal_lahir" : "1995-04-10",
+		"nilai" : 70
+	),
+	"5" => array(
+		"nama" : "Rahma",
+		"kelas" : "Node JS",
+		"alamat" : "Yogyakarta",
+		"tanggal_lahir" : "1993-09-15",
+		"nilai" : 85
+	)
+    );
+    $json = json_encode($data);
+    echo $json;
+?>
+https://tondanoweb.com/cara-melakukan-konversi-array-ke-json-dan-json-ke-array-pada-php/
 </body>
 </html>
