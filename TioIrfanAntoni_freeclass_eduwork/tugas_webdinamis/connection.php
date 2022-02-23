d77b4320283c3faf37ed82c93f1079cc1e0ab745
sityoy <tioirfanantoni@gmail.com>
2022-02-23 23:14:10 +0700

    Web Dinamis

--- TioIrfanAntoni_freeclass_eduwork/tugas_webdinamis/connection.php
+++ TioIrfanAntoni_freeclass_eduwork/tugas_webdinamis/connection.php
@@ -19,13 +19,13 @@ if (!$conn) {
 // echo "Connect Sukses Baby";
 // mysqli_close($conn);
 
-$sql = "SELECT * FROM pendaftaran";
+$sql = "SELECT * FROM customer LEFT JOIN pendaftaran ON pendaftaran.id_pendaftaran = type_mobil";
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0){
+if ($result->num_rows> 0){
     // output data of each row
     while($row = $result->fetch_assoc()){
-        echo "Customer: " . $row["id_customer"]. " - " . $row["jam_pendaftaran"]. "<br>";
+        echo "Customer: " . $row["nama"]. " - " . $row["type_mobil"]. "<br>";
     }
 } else {
     echo "0 results";
