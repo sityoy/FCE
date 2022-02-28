07a511aed98b11a54add7da4618f3ee60b0dd3ec
sityoy <tioirfanantoni@gmail.com>
2022-02-28 17:16:00 +0700

    update

--- TioIrfanAntoni_freeclass_eduwork/tugas_webdinamis/connect.php
+++ TioIrfanAntoni_freeclass_eduwork/tugas_webdinamis/connect.php
@@ -1,34 +1,34 @@
 <?php
 
-// use LDAP\Result;
+ use LDAP\Result;
 
 $servername = "localhost";
 $database = "db_perpus";
 $username = "root";
 $pass = "";
 
-// Create Connection
+//  Create Connection
 $mysqli = mysqli_connect($servername, $username, $pass, $database);
 
-// Check Connection
+//  Check Connection
 
-// if (!$mysqli) {
-//     die ("Connection Failed : " . mysqli_connect_error());
-// }
+ if (!$mysqli) {
+     die ("Connection Failed : " . mysqli_connect_error());
+ }
 
-// echo "Connect Sukses Baby";
-// mysqli_close($conn);
+ echo "Connect Sukses Baby";
+ mysqli_close($conn);
 
-// $sql = "SELECT * FROM pendaftaran";
-// $result = $conn->query($sql);
+ $sql = "SELECT * FROM pendaftaran";
+ $result = $conn->query($sql);
 
-// if ($result->num_rows > 0){
-//     // output data of each row
-//     while($row = $result->fetch_assoc()){
-//         echo "Customer: " . $row["id_customer"]. " - " . $row["jam_pendaftaran"]. "<br>";
-//     }
-// } else {
-//     echo "0 results";
-// }
-// $conn->close();
+ if ($result->num_rows > 0){
+    //   output data of each row
+     while($row = $result->fetch_assoc()){
+         echo "Customer: " . $row["id_customer"]. " - " . $row["jam_pendaftaran"]. "<br>";
+     }
+ } else {
+     echo "0 results";
+ }
+ $conn->close();
 ?>
